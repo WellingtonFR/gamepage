5f530cb7e3061eafdf072a590748b392b3c43cae
WellingtonFR <wellingtondfr@gmail.com>
2024-08-22 15:19:55 -0300

    finalizado o cabeçalho

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,7 +1,35 @@
+import logoImg from "/public/images/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+import { LiaGamepadSolid } from "react-icons/lia";
+
 export function Header() {
   return (
-    <header className="w-full h-28 bg-slate-100 text-black px-2">
-      <h1>TESTE</h1>
+    <header className="w-full h-28 bg-slate-200 text-black px-2">
+      <div className="max-w-screen-xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
+          <Link href="/" className="mr-8">
+            <Image src={logoImg} alt="Logo do site dalygames" quality={100} priority={true} className="w-full" />
+          </Link>
+
+          <Link href="/" className="font-bold transition-colors duration:1000ms hover:text-orange-600">
+            GAMES
+          </Link>
+          <Link href="/profile" className="font-bold transition-colors duration:1000ms hover:text-orange-600">
+            PERFIL
+          </Link>
+        </nav>
+
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/profile">
+            <LiaGamepadSolid
+              size={34}
+              className="text-slate-500 transition-colors duration:500ms hover:text-slate-800"
+            />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
