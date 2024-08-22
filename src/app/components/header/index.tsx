30d51d20d97afdf22dab3323911d1fc600ffb652
WellingtonFR <wellingtondfr@gmail.com>
2024-08-22 00:31:06 -0300

    Inicio da criação do header

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,3 +1,7 @@
 export function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <h1>TESTE</h1>
+    </header>
+  );
 }
