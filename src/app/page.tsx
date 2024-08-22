3d1030dbbe8a83622e24a4ed4a50514b5ad3fb08
WellingtonFR <wellingtondfr@gmail.com>
2024-08-22 15:46:46 -0300

    Inicio da criação de geração de indicação do jogo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
-import styles from "./page.module.css";
+import { Container } from "@/app/components/container";
+import { GameProps } from "@/app/utils/types/game";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+async function getDalyGame() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`);
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
+export default async function Home() {
+  const dalyGame: GameProps = await getDalyGame();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p> Início</p>
-      </div>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo pra você</h1>
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image
+              src={dalyGame.image_url}
+              alt={dalyGame.title}
+              priority={true}
+              quality={100}
+              width={100}
+              height={100}
+            />
+          </section>
+        </Link>
+      </Container>
     </main>
   );
 }
