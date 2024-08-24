b4d0e9313d6a7a7c3ec4991f7d74179ed4c33523
WellingtonFR <wellingtondfr@gmail.com>
2024-08-24 10:28:07 -0300

    Adiciona a pagina da busca de jogos

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,33 @@
+import { GameProps } from "@/app/utils/types/game";
+import { Container } from "@/app/components/container";
+import { Input } from "@/app/components/InputSearch";
+import { GameCard } from "@/app/components/GameCard";
+
+async function getData(title: string) {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`);
+    return res.json();
+  } catch (err) {
+    return null;
+  }
+}
+
+export default async function Search({ params: { title } }: { params: { title: string } }) {
+  const games: GameProps[] = await getData(title);
+
+  return (
+    <main className="w-full text-black">
+      <Container>
+        <Input />
+
+        <h1 className="font-bold text-xl mt-8 mb-5">Veja o que encontramos para você:</h1>
+
+        {!games && <p>Esse jogo não foi encontrado!...</p>}
+
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+          {games && games.map((item) => <GameCard key={item.id} data={item} />)}
+        </section>
+      </Container>
+    </main>
+  );
+}
